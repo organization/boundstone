dd9e7d085e5ec92b8c0d22257486b4ceb25d6586
SiYoung <win4362@gmail.com>
2019-09-09 17:27:36 +0900

    Update VRakLib

--- boundstone.v
+++ boundstone.v
@@ -4,9 +4,9 @@ import readline
 fn main() {
     println('Starting Server...')
     mut server := bstone.BoundstoneServer{
-        port: 19132
-        name: 'Server'
-        number_of_players: 100
+       port: 19132
+       name: 'Server'
+       number_of_players: 100
     }
     server.start()
     println('Server has been started')
